73b95c015edd8ff8355740ff9e78369e739d6b66
Dilip S <dilipsv.04@gmail.com>
2025-09-02 14:43:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,9 +12,10 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
-    },
+      },
       server: {
         host: '0.0.0.0',
         port: 3000
+      }
     };
 });
